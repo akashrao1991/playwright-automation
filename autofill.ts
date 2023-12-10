8be9ea53432ecc6d90f98a787f4d3005925975b5
akashrao1991 <akashrao1991@gmail.com>
2023-12-10 10:46:29 +0530

    removed awaits before page.locator and logging after filling each entry

--- autofill.ts
+++ autofill.ts
@@ -2,86 +2,105 @@
 
     const { chromium, devices } = await import("playwright");
 
-    // const BASE_URL = 'https://www.google.com/'
     const browser = await chromium.launch({ headless: false });
     const context = await browser.newContext(devices['Desktop Chrome']);
     const page = await context.newPage();
+    const URL = 'https://bukabantuan.bukalapak.com/form/175'
 
 
-    await page.goto('https://bukabantuan.bukalapak.com/form/175')
+    await page.goto(URL)
 
-    // await new Promise(r => setTimeout(r, 2000));
-
-    const name = await page.getByLabel('Nama (Name)')
+    const name = page.getByLabel('Nama (Name)')
     await name.fill('Akash Rao')
-
-    // await new Promise(r => setTimeout(r, 2000));
+    console.log('Filled Name')
 
     const email = page.getByLabel('Email', { exact: true })
     await email.fill('abc@xyz.com')
+    console.log('Filled Email ID')
 
     // await new Promise(r => setTimeout(r, 2000));
 
-    const intellectualProperty = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[3]/span/div/div/div[1]/div/input')
+    const intellectualProperty = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[3]/span/div/div/div[1]/div/input')
     await intellectualProperty.fill('Intellectual Property')
+    console.log('Filled Intellectual Property')
 
-    const registrationNumber = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[4]/span/div/div/div[1]/div/input')
+    const registrationNumber = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[4]/span/div/div/div[1]/div/input')
     await registrationNumber.fill('123456789')
+    console.log('Filled registrationNumber')
 
-    const ownerName = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[5]/span/div/div/div[1]/div/input')
+    const ownerName = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[5]/span/div/div/div[1]/div/input')
     await ownerName.fill('Akash Rao')
+    console.log('Filled ownerName')
 
-    const ownerOfIntellectualPropertyRadioButton = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[6]/span/div/div/label[1]/input')
+    const ownerOfIntellectualPropertyRadioButton = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[6]/span/div/div/label[1]/input')
     await ownerOfIntellectualPropertyRadioButton.check()
+    console.log('Filled ownerOfIntellectualPropertyRadioButton')    
 
-    const relationshipWithOwner = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[7]/span/div/div/div[1]/div/input')
+    const relationshipWithOwner = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[7]/span/div/div/div[1]/div/input')
     await relationshipWithOwner.fill('Sibling')
+    console.log('Filled relationshipWithOwner')    
 
-    const reportingCompanyName = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[8]/span/div/div/div[1]/div/input')
+    const reportingCompanyName = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[8]/span/div/div/div[1]/div/input')
     await reportingCompanyName.fill('ABC LLC')
+    console.log('Filled reportingCompanyName')    
 
-    const website = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[9]/span/div/div/div[1]/div/input')
+    const website = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[9]/span/div/div/div[1]/div/input')
     await website.fill('www.abc.xyz')
+    console.log('Filled website')    
 
-    const address = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[10]/span/div/div/div[1]/div/input')
+    const address = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[10]/span/div/div/div[1]/div/input')
     await address.fill('123,street name,near landmark,city - 123456 , country')
+    console.log('Filled address')    
 
-    const emailAddress = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[11]/span/div/div/div[1]/div/input')
+    const emailAddress = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[11]/span/div/div/div[1]/div/input')
     await emailAddress.fill('abc@xyz.com')
+    console.log('Filled emailAddress')    
 
-    const phoneNumber = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[12]/span/div/div/div[1]/div/input')
+    const phoneNumber = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[12]/span/div/div/div[1]/div/input')
     await phoneNumber.fill('1234567890')
+    console.log('Filled phoneNumber')    
 
-    const productLink = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[13]/span/div/div/div[1]/div/input')
+    const productLink = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[13]/span/div/div/div[1]/div/input')
     await productLink.fill('www.google.com')
+    console.log('Filled productLink')    
 
-    const problemDetails = await page.locator('//*[@id="body"]')
+    const problemDetails = page.locator('//*[@id="body"]')
     await problemDetails.fill('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    console.log('Filled problemDetails')    
 
-    const linksList = await page.locator('//*[@id="link_barang_banyak"]')
+    const linksList = page.locator('//*[@id="link_barang_banyak"]')
     await linksList.setInputFiles("Laporan_Pelanggaran_Hak_Kekayaan_Intelektual.xlsx")
+    console.log('Filled linksList')    
 
     const proofOfIntellectualProperty = page.locator('//*[@id="surat_kepemilikan_merek"]')
     await proofOfIntellectualProperty.setInputFiles('Task-DAE.pdf')
+    console.log('Filled proofOfIntellectualProperty')    
 
     const powerOfAttorney = await page.locator('//*[@id="bukti_surat_kuasa"]')
     await powerOfAttorney.setInputFiles('Task-DAE.pdf')
+    console.log('Filled powerOfAttorney')    
 
     const directSellingLicense = page.locator('//*[@id="bukti_surat_izin_usaha"]')
     await directSellingLicense.setInputFiles('Task-DAE.pdf')
+    console.log('Filled directSellingLicense')    
 
     const termsAndConditions = page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[20]/div[3]/label/input')
     await termsAndConditions.check()
+    console.log('Filled termsAndConditions')    
 
     // await new Promise(r => setTimeout(r, 20000));
 
     const submitButton = await page.locator('//*[@id="app-container"]/div[1]/div[2]/div[2]/div[2]/div/div/span/form/div/div[20]/button')
     await submitButton.click()
+    console.log('Clicked submitButton')    
 
+
+    console.log('started waiting for 20s before shutdown')    
     await new Promise(r => setTimeout(r, 20000));
+    console.log('done waiting for 20s')    
 
     await browser.close();
-    console.log('--------------------- Testing Successful')
+    console.log('Shutting Down....')
 
 })()
 
